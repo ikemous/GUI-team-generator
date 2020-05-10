004a8a356bc6cb0c5fab91a286f2fc45a89c4b3a
ike barranco <barranco.ike@outlook.com>
2020-05-10 02:14:15 -0600

    multiple server get methods have been added as well as extra buttons. modal has been started to being implemented as well

--- server.js
+++ server.js
@@ -3,40 +3,49 @@
 // =============================================================
 const express = require("express");
 const path = require("path");
-const app = require("./Assets/app");
-const script = require("./Assets/script");
+const app = require("./lib/app");
 // Sets up the Express App
 // =============================================================
 const server = express();
 const PORT = 3000;
 
 const employees = [
-
 ];
 
 
 // Sets up the Express app to handle data parsing
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
-server.use(express.static("Assets"));
+server.use('/Assets', express.static(__dirname + "/Assets"));
 
 // Basic route that sends the user first to the AJAX Page
 server.get("/", function(req, res) {
-    
     //Intialize 
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Basic route that sends the user first to the AJAX Page
 server.get("/team", function(req, res) {
-    
-    //Intialize 
-    res.sendFile(path.join(__dirname, "team.html"));
+    app.createFile(employees);
 });
+
+server.get("/pages/team", function(req, res){
+    //Intialize 
+    res.sendFile(path.join(__dirname, "/pages/team.html"));
+})
 
 // Basic route that sends the user first to the AJAX Page
 server.get("/api/employees", function(req, res) {
     return res.json(employees);
+});
+
+server.get("/api/employees/:employeeId", (req, res)=>{
+    const employeeId = req.params.employeeId;
+    for(const currentEmployee of employees)
+        if(employeeId === currentEmployee.id)
+            return res.json(currentEmployee);
+
+    return res.json(false);
 });
 
 server.post("/api/employees", function(req, res) {
